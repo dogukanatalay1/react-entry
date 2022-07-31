20d427d80d886fe8c7b1f56f3de6ddd1ca8254c7
Doğukan Atalay <dogukanatalay46@gmail.com>
2022-07-31 17:45:22 +0300

    styling entry

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useEffect } from 'react';
 import logo from './logo.svg'
+import styles from './App.module.css'
 
 function App() {
+
+  useEffect(()=> {
+    console.log(styles)
+    if(process.env.NODE_ENV === 'production') {
+      //analytic'i bilgilendir
+    }
+  }, [])
+
+
   return (
-    <div className="App">
-      <img src={logo} />
+    <div className={styles.App}>
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>
+        {process.env.REACT_APP_API_URL}
+      </p>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+         <img src={logo} alt="" />
+         <img src={logo} alt="" />
+        </>
+      )}
+      {/* <img src={logo} alt="" /> */}
     </div>
+   
   );
 }
 
 export default App;
+
+
